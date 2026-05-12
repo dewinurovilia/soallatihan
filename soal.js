1a98504072f9e6f581b45c59a7c5d21595046994
dewinurovilia <fauzy2327@gmail.com>
2026-05-12 07:24:47 +0700

    Add quiz questions to soal.js

---
+++ soal.js
@@ -0,0 +1,30 @@
+const questions = [
+
+  {
+    soal: "2 + 2 = ...",
+    opsiA: "3",
+    opsiB: "4",
+    opsiC: "5",
+    opsiD: "6",
+    jawaban: "B"
+  },
+
+  {
+    soal: "Ibu kota Indonesia adalah ...",
+    opsiA: "Bandung",
+    opsiB: "Jakarta",
+    opsiC: "Medan",
+    opsiD: "Surabaya",
+    jawaban: "B"
+  },
+
+  {
+    soal: "Planet terbesar adalah ...",
+    opsiA: "Mars",
+    opsiB: "Venus",
+    opsiC: "Jupiter",
+    opsiD: "Bumi",
+    jawaban: "C"
+  }
+
+];
